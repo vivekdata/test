d7fdfbd305ac8b4cc13191fcfeb52f7b3df4a1f7
vivekdata <1982vivekmate@gmail.com>
2021-05-06 05:49:52 +0530

    adding new file

---
+++ App.java
@@ -0,0 +1,15 @@
+package com.honeywell;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+		for(i=0;i<=400;i++)
+		{
+			i += 3;
+        	System.out.println( "print number: " + i);
+        	System.out.println( "print number: " + i);
+   		}
+	 }
+}
